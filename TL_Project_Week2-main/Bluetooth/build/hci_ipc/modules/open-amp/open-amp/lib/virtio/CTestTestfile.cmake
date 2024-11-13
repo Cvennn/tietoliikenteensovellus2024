213de6c289d3fad7e34c7ad20cce8193e1834562
Antti <antti.niiranen963@hotmail.fi>
2024-11-13 09:15:47 +0200

    korjattu ignore

---
+++ TL_Project_Week2-main/Bluetooth/build/hci_ipc/modules/open-amp/open-amp/lib/virtio/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/ncs/v2.7.0/modules/lib/open-amp/open-amp/lib/virtio
+# Build directory: C:/ncs/nordcodes/TLprojekti/tietoliikenteensovellus2024/TL_Project_Week2-main/Bluetooth/build/hci_ipc/modules/open-amp/open-amp/lib/virtio
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
