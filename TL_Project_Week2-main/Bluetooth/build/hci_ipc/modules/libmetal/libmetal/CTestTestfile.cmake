4e52c8ee759f5adcfd3440163cfa81aebcce9bb1
JuhoSai <juksu.sainmaa@hotmail.com>
2024-11-20 09:25:56 +0200

    httpkauttadataa

--- TL_Project_Week2-main/Bluetooth/build/hci_ipc/modules/libmetal/libmetal/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ncs/v2.7.0/modules/hal/libmetal/libmetal
-# Build directory: C:/tietoliikenneprojekti/tietoliikenteensovellus2024/TL_Project_Week2-main/Bluetooth/build/hci_ipc/modules/libmetal/libmetal
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
--- TL_Project_Week2-main/Bluetooth/build/hci_ipc/modules/open-amp/open-amp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ncs/v2.7.0/modules/lib/open-amp/open-amp
-# Build directory: C:/tietoliikenneprojekti/tietoliikenteensovellus2024/TL_Project_Week2-main/Bluetooth/build/hci_ipc/modules/open-amp/open-amp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
